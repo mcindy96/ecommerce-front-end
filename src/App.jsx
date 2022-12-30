99c0214b6a7f2931ec68a9f9f69c35145095ad14
Cindy Ma <macindy96@gmail.com>
2022-12-29 23:24:58 -0500

    initial commit for basic ecommerce website

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+import Cart from "./pages/Cart";
+
+const App = () => {
+  return <Cart />
+};
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://bubblysideoflife.com/wp-content/uploads/2020/10/cheers.png",
+        title: "PRE-SALE",
+        desc: "Get 30% off! Reserve your tickets early",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/originals/2d/14/76/2d1476db9b55044222dc3981846a70a2.jpg",
+        title: "EARLY ACCESS MERCHANDISE",
+        desc: "Show off your pride at the reunion!",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://images.squarespace-cdn.com/content/v1/5a8e08f7a803bbb6bf59cbaf/1586743609358-KI06F3H8IO4LG7F88Y50/image-asset.jpeg?format=1000w",
+        title: "BEER TASTING",
+        desc: "Add On Beer Tasting",
+        bg: "fbf0f4"
+    },
+]
+
+export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:3,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:4,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
